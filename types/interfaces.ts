1db08b335f10619fd3232fcfccd1cf4a4e7934f3
ritughiya <ritughiya@gmail.com>
2023-12-08 10:34:00 -0500

    type

--- types/interfaces.ts
+++ types/interfaces.ts
@@ -2,11 +2,14 @@ import { PortableTextBlock } from "sanity";
 
 export interface ProjectInfo {
     id: number,
+    projectLink: string,
     projectTitle: string,
     projectClients: PortableTextBlock[],
     projectDescription: PortableTextBlock[],
     projectServices: PortableTextBlock[],
     projectServicesList: PortableTextBlock[],
+    projectYear: string,
+    Collaboration: string,
     projectType: PortableTextBlock[],
     projectVideo: boolean,
     videoURL: URL,
