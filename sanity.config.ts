f2c0c8f74368d84ca5524b70e8dbd3df1a4e9a0c
ritughiya <ritughiya@gmail.com>
2024-01-02 12:29:23 -0500

    sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   projectId: '036bjsrb',
   dataset: 'production',
   basePath: "/studio",
-  apiVersion: "2023-12-14",
+  apiVersion: "2024-01-02",
 
 
   plugins: [deskTool(), visionTool(), media(), cloudinarySchemaPlugin()],
