1af2b25ffd72bb1846ccdc644d799c7f26f05148
ritughiya <ritughiya@gmail.com>
2024-01-08 15:31:47 -0500

    cache fix

--- sanity.config.ts
+++ sanity.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   projectId: '036bjsrb',
   dataset: 'production',
   basePath: "/studio",
-  apiVersion: "2024-01-04",
+  apiVersion: "2024-01-08",
 
 
   plugins: [deskTool(), visionTool(), media(), cloudinarySchemaPlugin()],
