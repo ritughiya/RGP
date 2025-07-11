ba9fc03e4ef6b3dbdaabcf0d2673c3905f975d89
Ritu Ghiya <ritughiya@gmail.com>
2025-07-11 10:06:17 -0400

    new capabilities

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer(props : any) {
      <div className="text-smmm lg:text-smm text-opacity-80 pt-md lg:pt-xxxll lg:mb-xxxll mb-xxxlll text-left lg:text-center font-sans text-sm tracking-[0.8px] w-full flex flex-col justify-center footerdesc">
               
                    <span className="mb-md">
-               Digital design & build, UX/UI interaction design, Web applications, Headless CMS, Headless e-commerce, Front & back-end development, Experimental development, GraphQL integration, Version control, Creative strategy, Digital concepts
+               Digital design & build, UX/UI interaction design, AI workflow integration, Web applications, Headless CMS, Headless e-commerce, Front & back-end development, Experimental development, Version control, Creative strategy, Digital concepts
               </span>
               <span className="mb-md">
               Stack: HTML, CSS, Javascript, React.js, Vue, Next, Node.js, Wordpress / PHP, Netlify, Shopify and Shopify Plus
